a161297a1489d28b8d4320e0b4fbb28087fbf5ba
Michkasov Roman <mihjob@list.ru>
2023-01-26 02:03:11 +0300

    main last

--- main.py
+++ main.py
@@ -16,16 +16,30 @@ print('МЕНЮ: ')
 for point in menuitems:
     print(point[0], point[1])
 
+routes = fn.ReadDataFromFile('route.txt')
+m = routes[0]
+n = m[2]
+print(n)
+
 number = input('Введите номер команды: ')
 if number == '1':
-    print(fn.PrintBus())
+    busData = fn.PrintBus()
+    for data in busData:
+        print(f'Id: {data[0]}, Госномер: {data[1]}')
 elif number == '2':
     fn.AddBus()
 elif number == '3':
-    print(fn.PrintDriver())
+    driverData = fn.PrintDriver()
+    for data in driverData:
+        print(f'Id: {data[0]}, ФИО водителя: {data[1]}')
 elif number == '4':
     fn.AddDriver()
 elif number == '5':
-    print(fn.PrintRoute())
+    dataRoute = fn.PrintRoute()
+    for data in dataRoute:
+        print(f'Id: {data[0]}, № автобуса: {data[1]}, id автобуса: {data[2]}, id водителя: {data[3]}')
+    print(f'Имя водителя: {fn.GetDriver()}')
 elif number == '6':
     fn.AddRoute()
+elif number == '7':
+    exit(0)
